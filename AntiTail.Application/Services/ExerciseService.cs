da99d0ccec5ef921164baa39d34f2036f172606e
mkstas <makarov.stn@gmail.com>
2025-11-20 00:34:35 +0300

    feat (persistence, application): craete exercise repository, service

---
+++ AntiTail.Application/Services/ExerciseService.cs
@@ -0,0 +1,41 @@
+﻿using AntiTail.Domain.Interfaces.Exercises;
+using AntiTail.Domain.Models;
+
+namespace AntiTail.Application.Services
+{
+    public class ExerciseService(
+        IExerciseRepository exerciseRepository) : IExerciseService
+    {
+        private readonly IExerciseRepository _exerciseRepository = exerciseRepository;
+
+        public async Task<Exercise> CreateExercise(long subjectId, string title, string description)
+        {
+            return await _exerciseRepository.Create(subjectId, title, description);
+        }
+
+        public async Task<List<Exercise>> GetAllExercises(long subjectId)
+        {
+            return await _exerciseRepository.GetAll(subjectId);
+        }
+
+        public async Task<Exercise> GetExerciseById(long id)
+        {
+            return await _exerciseRepository.GetById(id);
+        }
+
+        public async Task<bool> UpdateExercise(long id, string title, string description)
+        {
+            return await _exerciseRepository.Update(id, title, description);
+        }
+
+        public async Task<bool> UpdateExerciseStatus(long id, Status status)
+        {
+            return await _exerciseRepository.UpdateStatus(id, status);
+        }
+
+        public async Task<bool> DeleteExercise(long id)
+        {
+            return await _exerciseRepository.Delete(id);
+        }
+    }
+}
---
+++ AntiTail.Persistence/Repositories/ExerciseRepository.cs
@@ -0,0 +1,91 @@
+﻿using AntiTail.Domain.Interfaces.Exercises;
+using AntiTail.Domain.Models;
+using AntiTail.Infrastructure.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntiTail.Persistence.Repositories
+{
+    public class ExerciseRepository(AntiTailDbContext Context) : IExerciseRepository
+    {
+        private readonly AntiTailDbContext _context = Context;
+
+        public async Task<Exercise> Create(long subjectId, string title, string description)
+        {
+            var exercise = new Exercise(subjectId, title, description);
+
+            await _context.AddAsync(exercise);
+            await _context.SaveChangesAsync();
+
+            return exercise;
+        }
+
+        public async Task<List<Exercise>> GetAll(long subjectId)
+        {
+            var exercises = await _context.Exercises
+                .AsNoTracking()
+                .Where(e => e.SubjectId == subjectId)
+                .ToListAsync();
+
+            if (exercises.Count == 0)
+            {
+                throw new NotFoundException("Exercises are not found.");
+            }
+
+            return exercises;
+        }
+
+        public async Task<Exercise> GetById(long id)
+        {
+            return await _context.Exercises
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Id == id) ?? throw new NotFoundException("Exercise is not found.");
+        }
+
+        public async Task<bool> Update(long id, string title, string description)
+        {
+            var rows = await _context.Exercises
+                .Where(e => e.Id == id)
+                .ExecuteUpdateAsync(
+                    e => e
+                    .SetProperty(e => e.Title, title)
+                    .SetProperty(e => e.Description, description));
+
+            if (rows == 0)
+            {
+                throw new NotFoundException("Exercise is not found.");
+            }
+
+            return true;
+        }
+
+        public async Task<bool> UpdateStatus(long id, Status status)
+        {
+            var rows = await _context.Exercises
+                .Where(e => e.Id == id)
+                .ExecuteUpdateAsync(
+                    e => e
+                    .SetProperty(e => e.Status, status));
+
+            if (rows == 0)
+            {
+                throw new NotFoundException("Exercise is not found.");
+            }
+
+            return true;
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            var rows = await _context.Exercises
+                .Where(e => e.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (rows == 0)
+            {
+                throw new NotFoundException("Exercise is not found.");
+            }
+
+            return true;
+        }
+    }
+}
