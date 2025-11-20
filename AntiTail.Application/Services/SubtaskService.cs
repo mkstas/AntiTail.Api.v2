4372d6f00ba3408cb3d5b7e3a3e323f8c4ba78c1
mkstas <makarov.stn@gmail.com>
2025-11-21 01:59:31 +0300

    feat (application): create subtask service

---
+++ AntiTail.Application/Services/SubtaskService.cs
@@ -0,0 +1,40 @@
+﻿using AntiTail.Domain.Interfaces.Subtasks;
+using AntiTail.Domain.Models;
+
+namespace AntiTail.Application.Services
+{
+    public class SubtaskService(ISubtaskRepository subtaskRepository) : ISubtaskService
+    {
+        private readonly ISubtaskRepository _subtaskRepository = subtaskRepository;
+
+        public async Task<Subtask> CreateSubtask(long exerciseId, string title)
+        {
+            return await _subtaskRepository.Create(exerciseId, title);
+        }
+
+        public async Task<List<Subtask>> GetAllSubtasks(long exerciseId)
+        {
+            return await _subtaskRepository.GetAll(exerciseId);
+        }
+
+        public async Task<Subtask> GetSubtaskById(long id)
+        {
+            return await _subtaskRepository.GetById(id);
+        }
+
+        public async Task<bool> UpdateSubtask(long id, string title)
+        {
+            return await _subtaskRepository.Update(id, title);
+        }
+
+        public async Task<bool> UpdateSubtaskStatus(long id, Status status)
+        {
+            return await _subtaskRepository.UpdateStatus(id, status);
+        }
+
+        public async Task<bool> DeleteSubtask(long id)
+        {
+            return await _subtaskRepository.Delete(id);
+        }
+    }
+}
