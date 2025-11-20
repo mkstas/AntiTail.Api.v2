6500fc6dd3fa472f7a37e15fee133b6375d73d79
mkstas <makarov.stn@gmail.com>
2025-11-21 01:38:27 +0300

    feat (domain): create interfaces for subtask repository, service

---
+++ AntiTail.Domain/Interfaces/Subtasks/ISubtaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using AntiTail.Domain.Models;
+
+namespace AntiTail.Domain.Interfaces.Subtasks
+{
+    public interface ISubtaskRepository
+    {
+        Task<Exercise> Create(long exerciseId, string title);
+
+        Task<List<Subtask>> GetAll(long exerciseId);
+
+        Task<Subtask> GetById(long subtaskId);
+
+        Task<bool> Update(long subtaskId, string title);
+
+        Task<bool> UpdateStatus(long subtaskId, Status status);
+
+        Task<bool> Delete(long subtaskId);
+    }
+}
---
+++ AntiTail.Domain/Interfaces/Subtasks/ISubtaskService.cs
@@ -0,0 +1,19 @@
+﻿using AntiTail.Domain.Models;
+
+namespace AntiTail.Domain.Interfaces.Subtasks
+{
+    public interface ISubtaskService
+    {
+        Task<Exercise> CreateSubtask(long exerciseId, string title);
+
+        Task<List<Subtask>> GetAllSubtasks(long exerciseId);
+
+        Task<Subtask> GetSubtaskById(long subtaskId);
+
+        Task<bool> UpdateSubtask(long subtaskId, string title);
+
+        Task<bool> UpdateSubtaskStatus(long subtaskId, Status status);
+
+        Task<bool> DeleteSubtask(long subtaskId);
+    }
+}
